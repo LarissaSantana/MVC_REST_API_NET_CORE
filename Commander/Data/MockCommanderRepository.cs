ea4268144e1a365fbd2f5edb7e58bc63512dcb5c
larissa_santana <larissa.santana@napista.com.br>
2020-08-02 16:31:37 -0300

    implementando os métodos da interface ICommanderRepository

--- Commander/Data/MockCommanderRepository.cs
+++ Commander/Data/MockCommanderRepository.cs
@@ -7,12 +7,43 @@ namespace Commander.Data
     {
         public IEnumerable<Command> GetAppCommands()
         {
-            throw new System.NotImplementedException();
+            var commands = new List<Command>()
+            {
+                new Command
+                {
+                    Id = 0, 
+                    HowTo = "Boil an egg",
+                    Line = "Boil water",
+                    Platform = "Kettle & Pan"
+                },
+                new Command
+                {
+                    Id = 0, 
+                    HowTo = "Cut bread",
+                    Line = "Get a knife",
+                    Platform = "Knife & chopping board"
+                },
+                new Command
+                {
+                    Id = 0, 
+                    HowTo = "Make cup of tea",
+                    Line = "Place teabag in cup",
+                    Platform = "Kettle & cup"
+                }
+            };  
+
+            return commands;
         }
 
         public Command GetCommandById(int id)
         {
-            throw new System.NotImplementedException();
+            return new Command
+            {
+                Id = 0, 
+                HowTo = "Boil an egg",
+                Line = "Boil water",
+                Platform = "Kettle & Pan"
+            };
         }
     }
 }
